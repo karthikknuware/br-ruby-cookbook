a617cf33eeba1b50fcb6f0b1b25fca6aacb311e5
Jim Pruetting <jim.pruetting@cerner.com>
2016-02-09 11:12:58 -0600

    0.4.1

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name              'br_ruby'
-version           '0.4.0'
+version           '0.4.1'
 description       'Adds and removes Ruby versions using ruby_build'
 maintainer        'Jim Pruetting'
 maintainer_email  'core-team@builtbyrobots.com'
